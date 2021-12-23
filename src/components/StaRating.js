846c1be641992bff8241be55d32cfbc1e1ed6941
David <popadavid843@yahoo.com>
2021-12-23 18:58:36 +0200

    update

--- src/components/StaRating.js
+++ src/components/StaRating.js
@@ -1,52 +1,65 @@
+// import React from "react";
+// import { AiFillStar } from "react-icons/ai";
+// export default function StaRating(props) {
+//   const da = () => {
+//     if (props.rank == 5)
+//       return (
+//         <React.Fragment>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//         </React.Fragment>
+//       );
+//     else if (props.rank == 4)
+//       return (
+//         <React.Fragment>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//         </React.Fragment>
+//       );
+//     else if (props.rank == 3)
+//       return (
+//         <React.Fragment>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//         </React.Fragment>
+//       );
+//     else if (props.rank == 2)
+//       return (
+//         <React.Fragment>
+//           <AiFillStar color="gold"></AiFillStar>
+//           <AiFillStar color="gold"></AiFillStar>
+//         </React.Fragment>
+//       );
+//     else if (props.rank == 1)
+//       return (
+//         <React.Fragment>
+//           <AiFillStar color="red"></AiFillStar>
+//           <AiFillStar color="red"></AiFillStar>
+//         </React.Fragment>
+//       );
+//   };
+//   return (
+//     <div>
+//       <div>{da()}</div>
+//       <div className="timeline-dot"></div>
+//     </div>
+//   );
+// }
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
 export default function StaRating(props) {
-  const da = () => {
-    if (props.rank == 5)
-      return (
-        <React.Fragment>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-        </React.Fragment>
-      );
-    else if (props.rank == 4)
-      return (
-        <React.Fragment>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-        </React.Fragment>
-      );
-    else if (props.rank == 3)
-      return (
-        <React.Fragment>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-        </React.Fragment>
-      );
-    else if (props.rank == 2)
-      return (
-        <React.Fragment>
-          <AiFillStar color="gold"></AiFillStar>
-          <AiFillStar color="gold"></AiFillStar>
-        </React.Fragment>
-      );
-    else if (props.rank == 1)
-      return (
-        <React.Fragment>
-          <AiFillStar color="red"></AiFillStar>
-          <AiFillStar color="red"></AiFillStar>
-        </React.Fragment>
-      );
-  };
+  const stars = [...Array(parseInt(props.rank) || 0)].map((e, i) => (
+    <AiFillStar color="gold" key={i}></AiFillStar>
+  ));
   return (
     <div>
-      <div>{da()}</div>
+      <div>{stars}</div>
       <div className="timeline-dot"></div>
     </div>
   );
