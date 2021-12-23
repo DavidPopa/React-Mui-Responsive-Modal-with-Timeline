f4bebe81cc17b5db1cd9d1db76ddef7abbdb126b
David <popadavid843@yahoo.com>
2021-12-23 18:20:40 +0200

    starRating

---
+++ src/components/StaRating.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
+export default function StaRating(props) {
+  const da = () => {
+    if (props.rank == 4)
+      return (
+        <React.Fragment>
+          <AiFillStar color="gold"></AiFillStar>
+          <AiFillStar color="gold"></AiFillStar>
+          <AiFillStar color="gold"></AiFillStar>
+          <AiFillStar color="gold"></AiFillStar>
+        </React.Fragment>
+      );
+    else if (props.rank == 3)
+      return (
+        <React.Fragment>
+          <AiFillStar color="gold"></AiFillStar>
+          <AiFillStar color="gold"></AiFillStar>
+          <AiFillStar color="gold"></AiFillStar>
+        </React.Fragment>
+      );
+    else if (props.rank == 2)
+      return (
+        <React.Fragment>
+          <AiFillStar color="gold"></AiFillStar>
+          <AiFillStar color="gold"></AiFillStar>
+        </React.Fragment>
+      );
+    else if (props.rank == 1)
+      return (
+        <React.Fragment>
+          <AiFillStar color="red"></AiFillStar>
+          <AiFillStar color="red"></AiFillStar>
+        </React.Fragment>
+      );
+  };
+  return (
+    <div>
+      <div>{da()}</div>
+      <div className="timeline-dot"></div>
+    </div>
+  );
+}
--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Timeline.css";
-
+import StaRating from "./StaRating";
 const TimelineItem = (props) => (
   <div className="timeline-item">
-    <div className="timeline-dot"></div>
     <div className="timeline-date">{props.data.date}</div>
     <div className="timeline-content">
       <h3>{props.data.name}</h3>
-      <p>{props.data.rank}</p>
+      <p>
+        <StaRating rank={props.data.rank}></StaRating>
+      </p>
     </div>
   </div>
 );
