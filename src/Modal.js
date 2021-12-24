f0a509e0fa34e54b9bef52479b09ef06e405a7a3
David <popadavid843@yahoo.com>
2021-12-24 10:38:09 +0200

    update

--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import Modal from "@material-ui/core/Modal"
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
 import { AiOutlineClose } from "react-icons/ai";
 import TimelineItem from "./components/TimelineItem";
 import data from "./data.json";
@@ -8,32 +8,32 @@ import "./Modal.css";
 
 function getModalStyle() {
   return {
-    width: '100%',
-    maxWidth: '100vw',
-    maxHeight: '100%',
-    position: 'fixed',
-    top: '50%',
-    left:  '0%',
-    transform: 'translate(0, -50%)',
-    overflowY: 'auto'
+    width: "100%",
+    maxWidth: "100vw",
+    maxHeight: "100%",
+    position: "fixed",
+    top: "50%",
+    left: "0%",
+    transform: "translate(0, -50%)",
+    overflowY: "auto",
   };
 }
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles((theme) =>
   createStyles({
     paper: {
-      position: 'absolute',
+      position: "absolute",
       width: 1500,
       backgroundColor: theme.palette.background.paper,
-      border: '2px solid #000',
+      border: "2px solid #000",
       boxShadow: theme.shadows[5],
       padding: theme.spacing(2, 4, 3),
     },
     button: {
-        float: 'right',
-        backgroundcolor: 'red',
+      float: "right",
+      backgroundcolor: "red",
     },
-  }),
+  })
 );
 
 export default function ModalApp(props) {
@@ -42,7 +42,6 @@ export default function ModalApp(props) {
   const classes = useStyles();
   // getModalStyle is not a pure function, we roll the style only on the first render
   const [modalStyle] = React.useState(getModalStyle);
-
 
   useEffect(() => {
     filterData.filter((item) => {
@@ -56,32 +55,32 @@ export default function ModalApp(props) {
   }
   return (
     <div>
-      <Modal
-        open={props.isOpen}
-        onRequestClose={closeModal}
-      >
+      <Modal open={props.isOpen} onRequestClose={closeModal}>
         <div>
-  
-        <div className="modalBck"  style={modalStyle} className={classes.paper}>
-        <button className="btn" onClick={closeModal}>
-          <AiOutlineClose size="3rem" />
-        </button>
-          <div className="modalCont">
-            <div className="title">
-              <h1>{props.fullName}</h1>
-            </div>
-            <div className="body">
-              <div className="timeline-items">
-                {props.nameEvents.map((item) => {
-                  return <TimelineItem data={item} />;
-                })}
+          <div
+            className="modalBck"
+            style={modalStyle}
+            className={classes.paper}
+          >
+            <button className="btn" onClick={closeModal}>
+              <AiOutlineClose size="3rem" />
+            </button>
+            <div className="modalCont">
+              <div className="title">
+                <h1>{props.fullName}</h1>
               </div>
+              <div className="body">
+                <div className="timeline-items">
+                  {props.nameEvents.map((item) => {
+                    return <TimelineItem data={item} />;
+                  })}
+                </div>
+              </div>
+              <footer>
+                <p>David</p>
+              </footer>
             </div>
-            <footer>
-              <p>David</p>
-            </footer>
           </div>
-        </div>
         </div>
       </Modal>
     </div>
