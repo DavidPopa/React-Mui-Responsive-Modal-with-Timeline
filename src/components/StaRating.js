9d74de2b83cc0f1ea6f9b0812c8b9754db396837
David <popadavid843@yahoo.com>
2021-12-24 15:31:27 +0200

    MI StaRating

--- src/components/StaRating.js
+++ src/components/StaRating.js
@@ -1,18 +1,5 @@
-// import React from "react";
-// import { AiFillStar } from "react-icons/ai";
-// export default function StaRating(props) {
-//   const stars = [...Array(parseInt(props.rank) || 0)].map((e, i) => (
-//     <AiFillStar color="gold" key={i}></AiFillStar>
-//   ));
-//   return (
-//     <div>
-//       <div>{stars}</div>
-//       <div className="timeline-dot"></div>
-//     </div>
-//   );
-// }
 import React from "react";
-import { Rating } from "@mui/material";
+import { Rating } from "@material-ui/lab";
 export default function StaRating(props) {
   return (
     <div>
