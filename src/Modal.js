0abd48e64a8c53da54557b0f6c5aac0bcb4f9888
abran <alexandru.bran@nokia.com>
2021-12-24 08:00:13 +0200

    modal fix

--- src/Modal.js
+++ src/Modal.js
@@ -1,23 +1,50 @@
 import React, { useState, useEffect } from "react";
-import Modal from "react-modal";
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+// import Modal from "react-modal";
+import Modal from "@material-ui/core/Modal"
 import { AiOutlineClose } from "react-icons/ai";
 import TimelineItem from "./components/TimelineItem";
 import data from "./data.json";
 import "./Modal.css";
 
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: "95%",
-    height: "95%",
-    borderRadius: "10px 10px 10px 10px",
-  },
-};
+function getModalStyle() {
+  return {
+    width: '100%',
+    maxWidth: '100vw',
+    maxHeight: '100%',
+    position: 'fixed',
+    top: '50%',
+    left:  '0%',
+    transform: 'translate(0, -50%)',
+    overflowY: 'auto'
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      position: 'absolute',
+      width: 1500,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+    button: {
+        float: 'right',
+        backgroundcolor: 'red',
+    },
+  }),
+);
 
 export default function ModalApp(props) {
   const [filterData] = useState(data);
+
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+
+
   useEffect(() => {
     filterData.filter((item) => {
       if (item.id === props.id) {
@@ -31,14 +58,16 @@ export default function ModalApp(props) {
   return (
     <div>
       <Modal
-        isOpen={props.isOpen}
+        open={props.isOpen}
         onRequestClose={closeModal}
-        style={customStyles}
+        // style={customStyles}
       >
+        <div>
+  
+        <div className="modalBck"  style={modalStyle} className={classes.paper}>
         <button className="btn" onClick={closeModal}>
           <AiOutlineClose size="3rem" />
         </button>
-        <div className="modalBck">
           <div className="modalCont">
             <div className="title">
               <h1>{props.fullName}</h1>
@@ -54,6 +83,7 @@ export default function ModalApp(props) {
               <p>David</p>
             </footer>
           </div>
+        </div>
         </div>
       </Modal>
     </div>
