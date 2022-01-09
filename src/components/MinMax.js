bf2198a530f878d645d5fa46c2715716d0f6bbb9
David <popadavid843@yahoo.com>
2022-01-09 22:36:00 +0200

    corectare modal responsive

--- src/components/MinMax.js
+++ src/components/MinMax.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./MinMax";
 
 export default function TimelineItem(props) {
-  console.log(typeof props.da);
+  console.log(props.da[0]);
   return (
     <div className="vals">
       <p>{props.value.rank}</p>
